408bbab2f2e277d3ce8389dde513fe0e3af3f63f
Jorge1 <agullojorge@gmail.com>
2024-04-15 21:59:00 +0200

    Deleted unused directives

--- app/src/main/java/com/softyorch/famousquotes/MainActivity.kt
+++ app/src/main/java/com/softyorch/famousquotes/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
