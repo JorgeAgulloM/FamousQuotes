d799abc503fba06a5026c1119db6f39f2ebfa9a0
Jorge1 <agullojorge@gmail.com>
2024-04-15 21:58:33 +0200

    Example implementation with flavors buildVariants

--- app/src/main/java/com/softyorch/famousquotes/MainActivity.kt
+++ app/src/main/java/com/softyorch/famousquotes/MainActivity.kt
@@ -3,14 +3,24 @@ package com.softyorch.famousquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.softyorch.famousquotes.ui.theme.FamousQuotesTheme
+import com.softyorch.famousquotes.ui.theme.PrimaryColor
+import com.softyorch.famousquotes.ui.theme.SecondaryColor
+import com.softyorch.famousquotes.ui.theme.TertiaryColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +41,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val context = LocalContext.current
+
+    val appResourceName = BuildConfig.APP_TITLE
+    val intResource = context.resources.getIdentifier(appResourceName, "string", context.packageName)
+    val appTitle = stringResource(intResource)
+
+    Column {
+        Text(
+            text = "Hello $appTitle!",
+            modifier = modifier.background(color = PrimaryColor),
+            color = Color.White
+        )
+        Text(
+            text = "Hello $appTitle!",
+            modifier = modifier.background(color = SecondaryColor),
+            color = Color.White
+        )
+        Text(
+            text = "Hello $appTitle!",
+            modifier = modifier.background(color = TertiaryColor),
+            color = Color.White
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true)
