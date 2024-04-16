3bb71cba712614493b242e02039954ebdb61a35e
Jorge1 <agullojorge@gmail.com>
2024-04-16 22:16:25 +0200

    Getting quote

--- app/src/main/java/com/softyorch/famousquotes/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/home/HomeViewModel.kt
@@ -1,10 +1,41 @@
 package com.softyorch.famousquotes.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softyorch.famousquotes.domain.SelectRandomQuote
+import com.softyorch.famousquotes.domain.model.FamousQuoteModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val selectQuote: SelectRandomQuote,
+    private val dispatcherIO: CoroutineDispatcher = Dispatchers.IO
+): ViewModel() {
 
+    private val _uiState = MutableStateFlow(HomeState(quote = FamousQuoteModel("", "", "")))
+    val uiState: StateFlow<HomeState> = _uiState
+
+    init {
+        getQuote()
+    }
+
+    private fun getQuote() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true) }
+
+            val quote = withContext(dispatcherIO) {
+                selectQuote()
+            }
+
+            _uiState.update { it.copy(isLoading = false, quote = quote) }
+        }
+    }
 }
