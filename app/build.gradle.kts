898dfc21fb62ec77d170036f764076c009804ec3
Jorge1 <agullojorge@gmail.com>
2024-04-16 22:16:04 +0200

    Implemented runtimeCompose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,6 +141,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
