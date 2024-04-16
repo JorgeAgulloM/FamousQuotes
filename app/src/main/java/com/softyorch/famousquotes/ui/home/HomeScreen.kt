b373f4ceb1103c6e10dd017e23435bfacd769118
Jorge1 <agullojorge@gmail.com>
2024-04-16 22:17:09 +0200

    Implemented quote data

--- app/src/main/java/com/softyorch/famousquotes/ui/home/HomeScreen.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/home/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.softyorch.famousquotes.ui.home
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,20 +16,29 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.softyorch.famousquotes.R
 import com.softyorch.famousquotes.ui.admob.Banner
 import com.softyorch.famousquotes.ui.theme.PanelColor
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
+
+    val state: HomeState by viewModel.uiState.collectAsStateWithLifecycle()
+
     Box(contentAlignment = Alignment.Center) {
         BackgroundImage()
-        CardQuote()
+        CardQuote(body = state.quote.body, owner = state.quote.owner)
     }
 }
 
@@ -35,7 +46,7 @@ fun HomeScreen(viewModel: HomeViewModel) {
 fun BackgroundImage() {
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
-            painter = painterResource(R.drawable.bg_image_5),
+            painter = painterResource(R.drawable.bg_image_9),
             modifier = Modifier.fillMaxWidth().height(300.dp),
             contentDescription = "Background",
             contentScale = ContentScale.Crop
@@ -44,26 +55,36 @@ fun BackgroundImage() {
 }
 
 @Composable
-fun CardQuote() {
+fun CardQuote(body: String, owner: String) {
     Column(modifier = Modifier.fillMaxSize()) {
         Box(modifier = Modifier.weight(1f))
         ElevatedCard(
             modifier = Modifier.fillMaxWidth().weight(2f),
-            shape = MaterialTheme.shapes.extraLarge
+            shape = MaterialTheme.shapes.extraLarge,
         ) {
             Column(
-                modifier = Modifier.background(color = PanelColor).fillMaxSize(),
+                modifier = Modifier
+                    .border(
+                        BorderStroke(1.dp, Color.DarkGray),
+                        shape = MaterialTheme.shapes.extraLarge
+                    )
+                    .background(color = PanelColor)
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Spacer(modifier = Modifier.height(24.dp))
-                    TextHome(text = "Debes seguir tu camino!!", true)
+                    TextHome(
+                        text = "\"$body\"",
+                        true
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
                     Box(
                         modifier = Modifier.fillMaxWidth(),
                         contentAlignment = Alignment.CenterEnd
                     ) {
-                        TextHome(text = "\"Yorch, the creator\"")
+                        TextHome(text = "\"$owner\"")
                     }
                 }
                 Banner()
@@ -74,7 +95,10 @@ fun CardQuote() {
 
 @Composable
 fun TextHome(text: String, isBody: Boolean = false) {
-    val style =
-        if (isBody) MaterialTheme.typography.headlineSmall else MaterialTheme.typography.bodyLarge
-    Text(text = text, modifier = Modifier.padding(16.dp), style = style)
+    val style = if (isBody) MaterialTheme.typography.headlineSmall.copy(
+        textAlign = TextAlign.Center,
+        fontFamily = FontFamily.Cursive,
+        fontWeight = FontWeight.Bold
+    ) else MaterialTheme.typography.bodyLarge
+    Text(text = text, modifier = Modifier.padding(16.dp).fillMaxWidth(), style = style)
 }
