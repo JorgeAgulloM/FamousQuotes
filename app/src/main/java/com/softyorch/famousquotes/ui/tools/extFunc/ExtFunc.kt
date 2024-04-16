7782a02eeabf54266bb2464323a2ea715af108f5
Jorge1 <agullojorge@gmail.com>
2024-04-16 07:48:53 +0200

    Created getStrings by name exFunc

---
+++ app/src/main/java/com/softyorch/famousquotes/ui/tools/extFunc/ExtFunc.kt
@@ -0,0 +1,13 @@
+package com.softyorch.famousquotes.ui.tools.extFunc
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+@SuppressLint("DiscouragedApi")
+@Composable
+fun Context.getResourceString(name: String): String {
+    val intResource = this.resources.getIdentifier(name, "string", this.packageName)
+    return stringResource(intResource)
+}
