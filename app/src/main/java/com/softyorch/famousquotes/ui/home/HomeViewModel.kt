dbae67cdd8051ddb94a3dc28ce9b5fc467a23676
Jorge1 <agullojorge@gmail.com>
2024-04-16 07:48:24 +0200

    Created home view model

---
+++ app/src/main/java/com/softyorch/famousquotes/ui/home/HomeViewModel.kt
@@ -0,0 +1,10 @@
+package com.softyorch.famousquotes.ui.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(): ViewModel() {
+
+}
