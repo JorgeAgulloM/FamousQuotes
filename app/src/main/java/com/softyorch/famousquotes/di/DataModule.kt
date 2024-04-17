34b6057d888d363b503594aa70bd274b1aa6f1b0
Jorge1 <agullojorge@gmail.com>
2024-04-17 19:58:58 +0200

    Created data module

---
+++ app/src/main/java/com/softyorch/famousquotes/di/DataModule.kt
@@ -0,0 +1,31 @@
+package com.softyorch.famousquotes.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import com.softyorch.famousquotes.data.FakeDataBaseImplement
+import com.softyorch.famousquotes.domain.IFakeDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Singleton
+    @Provides
+    fun providesFakeDatabase(): IFakeDatabase = FakeDataBaseImplement()
+
+    @Singleton
+    @Provides
+    fun providesFirebaseFirestore(): FirebaseFirestore = Firebase.firestore
+
+    @Singleton
+    @Provides
+    fun providesFirebaseStorage(): FirebaseStorage = Firebase.storage
+}
