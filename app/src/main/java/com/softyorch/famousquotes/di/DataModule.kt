52a740f08c0ff8d28692180051b2a8487c86a060
Jorge1 <agullojorge@gmail.com>
2024-04-18 14:33:35 +0200

    Provides StorageService & Datastore interfaces

--- app/src/main/java/com/softyorch/famousquotes/di/DataModule.kt
+++ app/src/main/java/com/softyorch/famousquotes/di/DataModule.kt
@@ -1,15 +1,21 @@
 package com.softyorch.famousquotes.di
 
+import android.content.Context
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
+import com.softyorch.famousquotes.data.datastore.DatastoreImpl
 import com.softyorch.famousquotes.data.network.DatabaseServiceImpl
-import com.softyorch.famousquotes.domain.IDatabaseService
+import com.softyorch.famousquotes.data.network.StorageServiceImpl
+import com.softyorch.famousquotes.domain.interfaces.IDatabaseService
+import com.softyorch.famousquotes.domain.interfaces.IDatastore
+import com.softyorch.famousquotes.domain.interfaces.IStorageService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -27,6 +33,16 @@ object DataModule {
 
     @Singleton
     @Provides
-    fun providesDatabaseService(firestore: FirebaseFirestore, storage: FirebaseStorage):
-            IDatabaseService = DatabaseServiceImpl(firestore, storage)
+    fun providesDatabaseService(firestore: FirebaseFirestore):
+            IDatabaseService = DatabaseServiceImpl(firestore)
+
+    @Singleton
+    @Provides
+    fun providesStorageService(storage: FirebaseStorage):
+            IStorageService = StorageServiceImpl(storage)
+
+    @Singleton
+    @Provides
+    fun providesDatastore(@ApplicationContext context: Context):
+            IDatastore = DatastoreImpl(context)
 }
