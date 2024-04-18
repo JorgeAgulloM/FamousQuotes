f191da2d16d56ea926f6bf4b8957596377945d64
Jorge1 <agullojorge@gmail.com>
2024-04-18 07:43:19 +0200

    Provided IDatabaseService

--- app/src/main/java/com/softyorch/famousquotes/di/DataModule.kt
+++ app/src/main/java/com/softyorch/famousquotes/di/DataModule.kt
@@ -6,6 +6,8 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import com.softyorch.famousquotes.data.FakeDataBaseImplement
+import com.softyorch.famousquotes.data.network.DatabaseServiceImpl
+import com.softyorch.famousquotes.domain.IDatabaseService
 import com.softyorch.famousquotes.domain.IFakeDatabase
 import dagger.Module
 import dagger.Provides
@@ -28,4 +30,9 @@ object DataModule {
     @Singleton
     @Provides
     fun providesFirebaseStorage(): FirebaseStorage = Firebase.storage
+
+    @Singleton
+    @Provides
+    fun providesDatabaseService(firestore: FirebaseFirestore, storage: FirebaseStorage):
+            IDatabaseService = DatabaseServiceImpl(firestore, storage)
 }
