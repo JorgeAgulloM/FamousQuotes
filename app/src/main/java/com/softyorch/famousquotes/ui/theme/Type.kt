6a626a2f8c7e7a853d549dd99075bd7ffc4dab32
Jorge1 <agullojorge@gmail.com>
2024-04-18 12:50:12 +0200

    Created Personalized Font

--- app/src/main/java/com/softyorch/famousquotes/ui/theme/Type.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/theme/Type.kt
@@ -1,10 +1,32 @@
 package com.softyorch.famousquotes.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.softyorch.famousquotes.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Montserrat Alternates")
+val FontMontserrat = FontFamily(
+    Font(
+        googleFont = fontName,
+        fontProvider = provider,
+        weight = FontWeight.ExtraBold,
+        style = FontStyle.Italic
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +54,26 @@ val Typography = Typography(
     )
     */
 )
+
+val MyTypography = Typography(
+    labelMedium = TextStyle(
+        fontFamily = FontMontserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        color = Color.DarkGray
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontMontserrat,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 2.sp,
+        fontSize = 16.sp,
+        color = Color.DarkGray
+    ),
+    displayLarge = TextStyle(
+        fontFamily = FontMontserrat,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 24.sp,
+        color = Color.DarkGray
+    )
+)
+
