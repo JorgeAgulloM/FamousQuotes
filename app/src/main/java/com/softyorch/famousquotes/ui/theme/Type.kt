7f2667656c374e687480c88d0012c8b7f8e64ef9
Jorge1 <agullojorge@gmail.com>
2024-04-19 10:32:41 +0200

    Changed align to center

--- app/src/main/java/com/softyorch/famousquotes/ui/theme/Type.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/theme/Type.kt
@@ -1,6 +1,5 @@
 package com.softyorch.famousquotes.ui.theme
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -9,6 +8,7 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.softyorch.famousquotes.R
 
@@ -73,6 +73,7 @@ val MyTypography = Typography(
         fontFamily = FontMontserrat,
         fontWeight = FontWeight.ExtraBold,
         fontSize = 24.sp,
+        textAlign = TextAlign.Center,
         color = Color.DarkGray
     )
 )
