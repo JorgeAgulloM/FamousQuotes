facd39b463a67b581c66b7824358e1b2147dcfd5
Jorge1 <agullojorge@gmail.com>
2024-04-19 08:59:18 +0200

    Added new function

--- app/src/main/java/com/softyorch/famousquotes/domain/interfaces/IDatabaseService.kt
+++ app/src/main/java/com/softyorch/famousquotes/domain/interfaces/IDatabaseService.kt
@@ -3,6 +3,6 @@ package com.softyorch.famousquotes.domain.interfaces
 import com.softyorch.famousquotes.data.network.response.QuoteResponse
 
 interface IDatabaseService {
-
     suspend fun getQuote(id: String): QuoteResponse?
+    suspend fun getRandomQuote(): QuoteResponse?
 }
