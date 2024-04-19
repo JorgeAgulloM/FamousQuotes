5b721bf34fa87c544e9e099a5b38c57d4eb4b60e
Jorge1 <agullojorge@gmail.com>
2024-04-19 07:41:22 +0200

    Utils file

---
+++ app/src/main/java/com/softyorch/famousquotes/domain/utils/Utils.kt
@@ -0,0 +1,14 @@
+package com.softyorch.famousquotes.domain.utils
+
+import java.util.Calendar
+import java.util.TimeZone
+
+fun getTodayId(): String {
+    val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    calendar.set(Calendar.HOUR_OF_DAY, 0)
+    calendar.set(Calendar.MINUTE, 0)
+    calendar.set(Calendar.SECOND, 0)
+    calendar.set(Calendar.MILLISECOND, 0)
+    val oneHourInMillis = 1000 * 60 * 60
+    return (calendar.timeInMillis - oneHourInMillis).toString()
+}
