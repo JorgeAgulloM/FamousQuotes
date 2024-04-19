b0769fe18e802cdd65e928a82b571c96e165f4db
Jorge1 <agullojorge@gmail.com>
2024-04-19 07:40:56 +0200

    Renamed property

--- app/src/main/java/com/softyorch/famousquotes/data/network/response/QuoteResponse.kt
+++ app/src/main/java/com/softyorch/famousquotes/data/network/response/QuoteResponse.kt
@@ -5,7 +5,7 @@ import com.google.firebase.Timestamp
 data class QuoteResponse(
     val id: String = "",
     val owner: String = "",
-    val bodyList: List<String> = emptyList(),
+    val quote: List<String> = emptyList(),
     val date: Timestamp = Timestamp.now(),
     val imageUrl: String = "",
 )
