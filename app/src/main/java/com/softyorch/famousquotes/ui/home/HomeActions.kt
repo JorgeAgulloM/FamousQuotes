ba48a1a4d568d8b42302532fdd3399fbf1c77edb
Jorge1 <agullojorge@gmail.com>
2024-04-20 08:22:34 +0200

    Deleted unused directive

--- app/src/main/java/com/softyorch/famousquotes/ui/home/HomeActions.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/home/HomeActions.kt
@@ -1,7 +1,5 @@
 package com.softyorch.famousquotes.ui.home
 
-import android.net.wifi.hotspot2.pps.HomeSp
-
 sealed class HomeActions {
     data object Info: HomeActions()
     data object New: HomeActions()
