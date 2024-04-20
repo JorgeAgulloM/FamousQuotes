e2d285186336d34f8bf7fbeef8deae8cb5817f8a
Jorge1 <agullojorge@gmail.com>
2024-04-20 11:58:08 +0200

    Added getResources by name for no composables

--- app/src/main/java/com/softyorch/famousquotes/ui/utils/extFunc/ExtFunc.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/utils/extFunc/ExtFunc.kt
@@ -7,9 +7,17 @@ import androidx.compose.ui.res.stringResource
 
 @SuppressLint("DiscouragedApi")
 @Composable
-fun Context.getResourceString(name: String): String {
+fun Context.getResourceStringComposable(name: String): String {
     if (name.isBlank()) return name
 
     val intResource = this.resources.getIdentifier(name, "string", this.packageName)
     return stringResource(intResource)
+}
+
+@SuppressLint("DiscouragedApi")
+fun Context.getResourceStringNotComposable(name: String): String {
+    if (name.isBlank()) return name
+
+    val intResource = this.resources.getIdentifier(name, "string", this.packageName)
+    return getString(intResource)
 }
