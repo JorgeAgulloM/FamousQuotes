ec3f295447ce67db18ff2e384bf2f1c57f1862aa
Jorge1 <agullojorge@gmail.com>
2024-04-20 13:24:59 +0200

    Created interstitial state

---
+++ app/src/main/java/com/softyorch/famousquotes/ui/admob/InterstitialAdState.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023. File developed for DailyElectricCost App by Jorge Agulló Martín for SoftYorch
+ */
+
+package com.softyorch.famousquotes.ui.admob
+
+sealed class InterstitialAdState {
+    data object Loading : InterstitialAdState()
+    data object Showed : InterstitialAdState()
+    data object Impression : InterstitialAdState()
+    data object Clicked : InterstitialAdState()
+    data object Close : InterstitialAdState()
+    data object Error : InterstitialAdState()
+}
