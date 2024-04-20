ab9a9517759548aae25b512f13f4637e4951c384
Jorge1 <agullojorge@gmail.com>
2024-04-20 11:58:28 +0200

    Deleted image property

--- app/src/main/java/com/softyorch/famousquotes/ui/home/HomeState.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/home/HomeState.kt
@@ -4,6 +4,5 @@ import com.softyorch.famousquotes.domain.model.FamousQuoteModel
 
 data class HomeState(
     val quote: FamousQuoteModel,
-    val image: String = "",
     val isLoading: Boolean = false,
 )
