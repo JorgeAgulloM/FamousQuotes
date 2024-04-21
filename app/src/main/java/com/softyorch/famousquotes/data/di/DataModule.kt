9a2bd9e11a211a09c6706873e2338444e0970346
Jorge1 <agullojorge@gmail.com>
2024-04-21 18:35:50 +0200

    Split module

--- app/src/main/java/com/softyorch/famousquotes/data/di/DataModule.kt
+++ app/src/main/java/com/softyorch/famousquotes/data/di/DataModule.kt
@@ -2,13 +2,13 @@ package com.softyorch.famousquotes.data.di
 
 import android.content.Context
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
 import com.softyorch.famousquotes.data.datastore.DatastoreImpl
+import com.softyorch.famousquotes.data.network.ConfigServiceImpl
 import com.softyorch.famousquotes.data.network.DatabaseServiceImpl
 import com.softyorch.famousquotes.data.network.StorageServiceImpl
+import com.softyorch.famousquotes.domain.interfaces.IConfigService
 import com.softyorch.famousquotes.domain.interfaces.IDatabaseService
 import com.softyorch.famousquotes.domain.interfaces.IDatastore
 import com.softyorch.famousquotes.domain.interfaces.IStorageService
@@ -25,14 +25,6 @@ object DataModule {
 
     @Singleton
     @Provides
-    fun providesFirebaseFirestore(): FirebaseFirestore = Firebase.firestore
-
-    @Singleton
-    @Provides
-    fun providesFirebaseStorage(): FirebaseStorage = Firebase.storage
-
-    @Singleton
-    @Provides
     fun providesDatabaseService(firestore: FirebaseFirestore):
             IDatabaseService = DatabaseServiceImpl(firestore)
 
@@ -45,4 +37,9 @@ object DataModule {
     @Provides
     fun providesDatastore(@ApplicationContext context: Context):
             IDatastore = DatastoreImpl(context)
+
+    @Singleton
+    @Provides
+    fun providesConfigService(remoteConfig: FirebaseRemoteConfig):
+            IConfigService = ConfigServiceImpl(remoteConfig)
 }
---
+++ app/src/main/java/com/softyorch/famousquotes/data/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.softyorch.famousquotes.data.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import com.softyorch.famousquotes.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun providesFirebaseFirestore(): FirebaseFirestore = Firebase.firestore
+
+    @Singleton
+    @Provides
+    fun providesFirebaseStorage(): FirebaseStorage = Firebase.storage
+
+    @Singleton
+    @Provides
+    fun providesRemoteConfig(): FirebaseRemoteConfig = Firebase.remoteConfig.apply {
+        setConfigSettingsAsync(
+            remoteConfigSettings { minimumFetchIntervalInSeconds = 3600 }
+        )
+        setDefaultsAsync(R.xml.remote_config_defaults)
+        fetchAndActivate()
+    }
+}
