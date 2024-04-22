ded6613b6e398730be7d369b24c53bbdf7ddd9e2
Jorge1 <agullojorge@gmail.com>
2024-04-22 09:31:48 +0200

    Renamed extFunc

--- app/src/main/java/com/softyorch/famousquotes/core/Send.kt
+++ app/src/main/java/com/softyorch/famousquotes/core/Send.kt
@@ -3,13 +3,13 @@ package com.softyorch.famousquotes.core
 import android.content.Context
 import android.content.Intent
 import com.softyorch.famousquotes.BuildConfig
-import com.softyorch.famousquotes.ui.utils.extFunc.getResourceStringNotComposable
+import com.softyorch.famousquotes.ui.utils.extFunc.getResourceString
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class Send @Inject constructor(@ApplicationContext private val context: Context) {
 
-    private val name = context.getResourceStringNotComposable(BuildConfig.APP_TITLE)
+    private val name = context.getResourceString(BuildConfig.APP_TITLE)
 
     fun sendDataTo(data: String) {
         val sendFrom = "Compartir desde"
