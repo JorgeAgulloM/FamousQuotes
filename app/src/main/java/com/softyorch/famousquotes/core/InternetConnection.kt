f540f2a451ed32c7f52c1eefd98dc732e9537825
Jorge1 <agullojorge@gmail.com>
2024-04-23 10:12:50 +0200

    Created new InternetConnection class

---
+++ app/src/main/java/com/softyorch/famousquotes/core/InternetConnection.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023-2024. File developed for DailyElectricCost App by Jorge Agulló Martín for SoftYorch
+ */
+
+package com.softyorch.famousquotes.core
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class InternetConnection @Inject constructor(@ApplicationContext private val context: Context) {
+
+    private var isConnected: Boolean = false
+
+    fun isConnected(): Boolean = isConnected
+
+    fun isConnectedFlow(): Flow<Boolean> = startConnectionCallback()
+
+    private fun startConnectionCallback(): Flow<Boolean> = callbackFlow {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                isConnected = true
+                channel.trySendBlocking(true)
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                isConnected = false
+                channel.trySendBlocking(false)
+            }
+        }
+
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        }
+    }
+}
