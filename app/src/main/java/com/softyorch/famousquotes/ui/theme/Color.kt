f4768c400edf65bb68e1b89a2d3ab0da986296ee
Jorge1 <agullojorge@gmail.com>
2024-04-24 09:08:55 +0200

    Reconfigured colors

--- app/src/main/java/com/softyorch/famousquotes/ui/theme/Color.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/theme/Color.kt
@@ -13,20 +13,33 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+/** // Configured colors in gradle flavors
+val HistoricalPrimary = Color(0xFFFFB005)
+val HistoricalSecondary = Color(0xFFFFD966)
+val HistoricalBackground = Color(0xFF8ECAE6)
+
+val BiblicalPrimary = Color(0xFF3182BD)
+val BiblicalSecondary = Color(0xFF6BAED6)
+val BiblicalBackground = Color(0xFFC6DBEF)
+
+val UpliftingPrimary = Color(0xFF99CE00)
+val UpliftingSecondary = Color(0xFFBAD56B)
+val UpliftingBackground = Color(0xFF88ABC2)
+*/
+
+val WhiteSmoke = Color(0xFFE6E6E6)
+
 val PrimaryColor = Color(BuildConfig.PRIMARY_COLOR)
 val SecondaryColor = Color(BuildConfig.SECONDARY_COLOR)
-val TertiaryColor = Color(BuildConfig.TERTIARY_COLOR)
-val BackgroundColor = Color(0xFFC1B79A)
-val PanelColor = Color(BuildConfig.PANEL_COLOR)
-val AuxColor = Color(BuildConfig.AUX_COLOR)
+val BackgroundColor = Color(BuildConfig.BACKGROUND_COLOR)
 
 @Composable
 fun brushBackGround(): Brush = Brush.linearGradient(
     listOf(
         BackgroundColor,
-        Color.White,
+        WhiteSmoke,
         BackgroundColor,
-        Color.White,
+        WhiteSmoke,
         BackgroundColor
     )
 )
