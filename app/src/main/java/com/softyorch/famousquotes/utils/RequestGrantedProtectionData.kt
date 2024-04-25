b8b5d28e8654f6a1aaa62a2f16c00b2e3b5b5832
Jorge1 <agullojorge@gmail.com>
2024-04-25 19:28:48 +0200

    Integrated ads consent

---
+++ app/src/main/java/com/softyorch/famousquotes/utils/RequestGrantedProtectionData.kt
@@ -0,0 +1,52 @@
+package com.softyorch.famousquotes.utils
+
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.UserMessagingPlatform
+import com.softyorch.famousquotes.ui.mainActivity.MainActivity
+import java.util.concurrent.atomic.AtomicBoolean
+
+class RequestGrantedProtectionData(private val mainActivity: MainActivity) {
+
+    private lateinit var consentInformation: ConsentInformation
+    private var isMobileAdsInitializedCalled = AtomicBoolean(false)
+    private val consent = "Consent"
+    private val notConsent = "Not consent"
+
+    fun get(callBack: (String) -> Unit) {
+        if (isMobileAdsInitializedCalled.get()) {
+            writeLog(LevelLog.INFO, "[RequestGrantedProtectionData] -> $consent")
+            return
+        }
+
+        val params: ConsentRequestParameters = ConsentRequestParameters
+            .Builder()
+            .setTagForUnderAgeOfConsent(false)
+            .build()
+
+        consentInformation = UserMessagingPlatform.getConsentInformation(mainActivity)
+        consentInformation.requestConsentInfoUpdate(
+            mainActivity,
+            params,
+            {
+                UserMessagingPlatform.loadAndShowConsentFormIfRequired(mainActivity) {
+                    if (consentInformation.canRequestAds()) {
+                        isMobileAdsInitializedCalled.set(true)
+                        writeLog(LevelLog.INFO, "[RequestGrantedProtectionData] -> $consent")
+                        callBack(consent)
+                    } else {
+                        isMobileAdsInitializedCalled.set(false)
+                        writeLog(LevelLog.INFO, "[RequestGrantedProtectionData] -> $notConsent")
+                        callBack(notConsent)
+                    }
+                }
+            },
+            {
+                writeLog(
+                    LevelLog.INFO,
+                    "[RequestGrantedProtectionData] -> Error: ${it.errorCode} - ${it.message}"
+                )
+            }
+        )
+    }
+}
