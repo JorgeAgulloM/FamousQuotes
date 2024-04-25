2c31f28dfdea259cd562955dfe9bcebed3c5bb2c
Jorge1 <agullojorge@gmail.com>
2024-04-25 06:03:43 +0200

    Implemented firebase messaging library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -147,6 +147,7 @@ dependencies {
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-storage-ktx")
     implementation("com.google.firebase:firebase-config-ktx")
