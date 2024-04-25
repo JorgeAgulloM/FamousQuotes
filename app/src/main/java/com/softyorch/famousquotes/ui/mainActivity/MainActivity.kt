cbeea3ae37f98cacc1e006b84fb98639a04e6d7f
Jorge1 <agullojorge@gmail.com>
2024-04-25 05:16:33 +0200

    Implemented integrated updates

--- app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainActivity.kt
@@ -10,6 +10,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
+import com.google.android.play.core.ktx.isImmediateUpdateAllowed
+import com.softyorch.famousquotes.BuildConfig
 import com.softyorch.famousquotes.ui.home.HomeScreen
 import com.softyorch.famousquotes.ui.home.HomeViewModel
 import com.softyorch.famousquotes.ui.theme.FamousQuotesTheme
@@ -19,9 +26,16 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var appUpdateManager: AppUpdateManager
+    private val appUpdateOptions = AppUpdateOptions.defaultOptions(AppUpdateType.FLEXIBLE)
+    private val channel = 1111
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        appUpdateManager = AppUpdateManagerFactory.create(applicationContext)
+        if (!BuildConfig.DEBUG) checkForAppUpdates()
+
         setContent {
             FamousQuotesTheme {
                 viewModel = hiltViewModel<MainViewModel>()
@@ -39,20 +53,41 @@ class MainActivity : ComponentActivity() {
                                 AlertState.Update -> viewModel.goToUpdateApp()
                             }
                         }
+
                         else -> {}
                     }
                 }
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        if (!BuildConfig.DEBUG) appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
+            if (info.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                appUpdateManager.startUpdateFlowForResult(info, this, appUpdateOptions, channel)
+            }
+        }
+    }
+
+    private fun checkForAppUpdates() {
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { info ->
+            val isUpdateAvailable = info.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+            val isUpdateAllowed = info.isImmediateUpdateAllowed
+            if (isUpdateAvailable && isUpdateAllowed) {
+                appUpdateManager.startUpdateFlowForResult(info, this, appUpdateOptions, channel)
+            }
+        }
+    }
 }
 
-//Mover textos a strings resources
-//Cinco idiomas
-//Modificar icono Uplifting flavor
-//Revisar colores en todos los flavors
-//Mejorar la animación de entrada de la imagen
+
 //Implementar actualziaciones automáticas
+//Preparar recepción de notificaciones
+//Implementar "Like" + recuento + lista de likes????????
+//Traducir las frases que ya tengo
+//Generar imagenes
+//Generar script python para cargar frases a firebase
 //Añadir Tests unitarios
 //Añadir Tests de implementación
 //Añadir CD/CI con github
