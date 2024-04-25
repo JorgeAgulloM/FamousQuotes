1b696c0bc3b49c52d5395fa0f38de65dcd67a2b1
Jorge1 <agullojorge@gmail.com>
2024-04-25 13:59:56 +0200

    Created use cases

---
+++ app/src/main/java/com/softyorch/famousquotes/domain/useCases/quoteLikes/SetQuoteLike.kt
@@ -0,0 +1,12 @@
+package com.softyorch.famousquotes.domain.useCases.quoteLikes
+
+import com.softyorch.famousquotes.domain.interfaces.IDatabaseService
+import com.softyorch.famousquotes.domain.model.LikesDTO
+import com.softyorch.famousquotes.domain.model.LikesDTO.Companion.toData
+import javax.inject.Inject
+
+class SetQuoteLike @Inject constructor(private val dbService: IDatabaseService) {
+    suspend operator fun invoke(updateLikes: LikesDTO) {
+        dbService.likeDislikeQuote(updateLikes.toData())
+    }
+}
