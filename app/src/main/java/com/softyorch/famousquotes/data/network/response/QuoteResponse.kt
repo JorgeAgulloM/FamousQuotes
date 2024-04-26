b9c48f52197f1d3c84d019e5e7b41ef292c35f67
Jorge1 <agullojorge@gmail.com>
2024-04-26 19:21:17 +0200

    Changed owner property type

--- app/src/main/java/com/softyorch/famousquotes/data/network/response/QuoteResponse.kt
+++ app/src/main/java/com/softyorch/famousquotes/data/network/response/QuoteResponse.kt
@@ -4,7 +4,7 @@ import com.google.firebase.Timestamp
 
 data class QuoteResponse(
     val id: String = "",
-    val owner: String = "",
+    val owner: List<String> = emptyList(),
     val quote: List<String> = emptyList(),
     val date: Timestamp = Timestamp.now(),
     val imageUrl: String = "",
