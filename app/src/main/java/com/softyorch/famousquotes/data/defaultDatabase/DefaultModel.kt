8dbc61028f55e6242a916efe2a1c4d8f998520a3
Jorge1 <agullojorge@gmail.com>
2024-05-21 09:21:26 +0200

    Added property

--- app/src/main/java/com/softyorch/famousquotes/data/defaultDatabase/DefaultModel.kt
+++ app/src/main/java/com/softyorch/famousquotes/data/defaultDatabase/DefaultModel.kt
@@ -3,4 +3,5 @@ package com.softyorch.famousquotes.data.defaultDatabase
 data class DefaultModel(
     val owner: String = "",
     val quote: List<String> = emptyList(),
+    val imageUrl: String = "",
 )
