198f610c499905819461ab16e2fd460a5dbca92e
Jorge1 <agullojorge@gmail.com>
2024-05-21 09:21:50 +0200

    Added interface property

--- app/src/main/java/com/softyorch/famousquotes/domain/interfaces/IDefaultDatabase.kt
+++ app/src/main/java/com/softyorch/famousquotes/domain/interfaces/IDefaultDatabase.kt
@@ -3,5 +3,6 @@ package com.softyorch.famousquotes.domain.interfaces
 import com.softyorch.famousquotes.data.defaultDatabase.DefaultModel
 
 interface IDefaultDatabase {
-    fun getDefaultDatabase(): List<DefaultModel>
+    fun getDefaultQuote(): DefaultModel
+    fun getDefaultImage(): String
 }
