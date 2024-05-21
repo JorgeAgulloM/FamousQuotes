8b69907c196dac039bf9986baba2f06196061ed1
Jorge1 <agullojorge@gmail.com>
2024-05-21 12:17:41 +0200

    Added timeOut

--- app/src/main/java/com/softyorch/famousquotes/data/network/StorageServiceImpl.kt
+++ app/src/main/java/com/softyorch/famousquotes/data/network/StorageServiceImpl.kt
@@ -3,14 +3,21 @@ package com.softyorch.famousquotes.data.network
 import com.google.firebase.storage.FirebaseStorage
 import com.softyorch.famousquotes.domain.interfaces.IStorageService
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withTimeoutOrNull
 import javax.inject.Inject
-class StorageServiceImpl @Inject constructor(private val storage: FirebaseStorage) :
-    IStorageService {
+class StorageServiceImpl @Inject constructor(
+    private val storage: FirebaseStorage
+) : IStorageService {
+
+    companion object {
+        private const val TIMEOUT: Long = 4000L
+    }
 
     override suspend fun getImage(url: String): String? = try {
-        storage.reference.child(url).downloadUrl.await().toString()
+        withTimeoutOrNull(TIMEOUT) {
+            storage.reference.child(url).downloadUrl.await().toString()
+        }
     } catch (ex: Exception) {
         null
     }
 }
-
