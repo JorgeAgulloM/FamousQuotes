c7dc445fdc5835f42f46dff2dfb9d11899ee2d27
Jorge1 <agullojorge@gmail.com>
2024-05-22 13:28:04 +0200

    Added semantic on Dialogs

--- app/src/main/java/com/softyorch/famousquotes/ui/home/components/NoConnectionDialog.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/home/components/NoConnectionDialog.kt
@@ -3,7 +3,6 @@ package com.softyorch.famousquotes.ui.home.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -16,6 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.text
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import com.softyorch.famousquotes.R
 import com.softyorch.famousquotes.ui.theme.WhiteSmoke
@@ -24,7 +26,11 @@ import com.softyorch.famousquotes.ui.theme.brushBackGround
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoConnectionDialog(onAction: () -> Unit) {
-    BasicAlertDialog(onDismissRequest = { onAction() }) {
+    val infoText = stringResource(R.string.main_info_dialog_connection)
+
+    BasicAlertDialog(onDismissRequest = { onAction() }, modifier = Modifier.semantics {
+        text = AnnotatedString(text = infoText)
+    }) {
         val shape = MaterialTheme.shapes.extraLarge
         Box(
             modifier = Modifier.fillMaxWidth().background(
@@ -38,14 +44,13 @@ fun NoConnectionDialog(onAction: () -> Unit) {
                     .background(brush = brushBackGround(), shape = shape)
                     .padding(16.dp)
             ) {
-                val text = stringResource(R.string.main_info_dialog_connection)
                 Icon(
                     imageVector = Icons.Outlined.WifiOff,
-                    contentDescription = text,
+                    contentDescription = infoText,
                     modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp),
                     tint = MaterialTheme.colorScheme.error
                 )
-                TextInfo(text)
+                TextInfo(infoText)
             }
         }
     }
