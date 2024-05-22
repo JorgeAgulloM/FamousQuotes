710e651207e267906baf6ac64465a1769067cf55
Jorge1 <agullojorge@gmail.com>
2024-05-22 13:29:00 +0200

    Deleted unused directives.

--- app/src/main/java/com/softyorch/famousquotes/ui/home/components/InfoDialog.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/home/components/InfoDialog.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.text
 import androidx.compose.ui.text.AnnotatedString
