59f932370173425683d8d228c75659f118bee4f8
Jorge1 <agullojorge@gmail.com>
2024-05-23 07:56:16 +0200

    Deleted files

--- app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainState.kt
+++
@@ -1,7 +0,0 @@
-package com.softyorch.famousquotes.ui.mainActivity
-
-sealed class MainState {
-    data object Home: MainState()
-    data object Loading: MainState()
-    data object TimeToUpdate: MainState()
-}
