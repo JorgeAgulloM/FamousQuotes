8c912878dd5b5f0e4d86284912b239e47f307101
Jorge1 <agullojorge@gmail.com>
2024-05-23 15:38:36 +0200

    Added navigation manager

---
+++ app/src/main/java/com/softyorch/famousquotes/ui/navigation/NavigationManager.kt
@@ -0,0 +1,25 @@
+package com.softyorch.famousquotes.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.softyorch.famousquotes.ui.screens.home.HomeScreen
+import com.softyorch.famousquotes.ui.screens.home.HomeViewModel
+import com.softyorch.famousquotes.ui.screens.splash.SplashScreen
+
+@Composable
+fun NavigationManager(navHost: NavHostController = rememberNavController()) {
+    val homeViewModel = hiltViewModel<HomeViewModel>()
+
+    NavHost(navController = navHost, startDestination = NavigationRoutes.SplashScreen.route) {
+        composable(route = NavigationRoutes.SplashScreen.route) {
+            SplashScreen(navHost = navHost)
+        }
+        composable(route = NavigationRoutes.HomeScreen.route) {
+            HomeScreen(navHost = navHost, viewModel = homeViewModel)
+        }
+    }
+}
