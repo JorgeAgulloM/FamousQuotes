268ed967fae67706499198c0af9fcbcf1e3f02b4
Jorge1 <agullojorge@gmail.com>
2024-05-23 15:39:56 +0200

    Deleted unused parameters and functions

--- app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainViewModel.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainViewModel.kt
@@ -3,7 +3,6 @@ package com.softyorch.famousquotes.ui.mainActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.softyorch.famousquotes.core.Intents
-import com.softyorch.famousquotes.domain.useCases.GetTodayQuote
 import com.softyorch.famousquotes.domain.useCases.TimeToUpdate
 import com.softyorch.famousquotes.utils.LevelLog
 import com.softyorch.famousquotes.utils.writeLog
@@ -21,10 +20,9 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val timeToUpdate: TimeToUpdate,
     private val intents: Intents,
-    private val selectQuote: GetTodayQuote,
     private val dispatcherIO: CoroutineDispatcher = Dispatchers.IO,
 ): ViewModel() {
-    private val _uiState = MutableStateFlow<MainState>(MainState.Loading)
+    private val _uiState = MutableStateFlow<MainState>(MainState.Home)
     val mainState: StateFlow<MainState> = _uiState
 
     init {
@@ -43,28 +41,7 @@ class MainViewModel @Inject constructor(
                 timeToUpdate()
             }
             writeLog(LevelLog.INFO, "Is time to Update?: $needUpdateApp")
-            _uiState.update {
-                startGettingQuote()
-                if (needUpdateApp) {
-                    MainState.TimeToUpdate
-                } else {
-                    MainState.Home
-                }
-            }
-        }
-    }
-
-    private fun startGettingQuote() {
-        viewModelScope.launch {
-            withContext(dispatcherIO) {
-                try {
-                    selectQuote()
-                } catch (ex: Exception) {
-                    writeLog(LevelLog.ERROR, "Error starting: ${ex.cause}")
-                }
-            }
-
-            _uiState.update { MainState.Home }
+            if (needUpdateApp) _uiState.update { MainState.TimeToUpdate }
         }
     }
 }
