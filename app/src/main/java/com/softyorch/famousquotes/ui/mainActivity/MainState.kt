ce17b718cdb9c443bbadc671f3f3a5622276ad15
Jorge1 <agullojorge@gmail.com>
2024-05-23 15:40:07 +0200

    Deleted unused state

--- app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainState.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainState.kt
@@ -2,6 +2,5 @@ package com.softyorch.famousquotes.ui.mainActivity
 
 sealed class MainState {
     data object TimeToUpdate: MainState()
-    data object Loading: MainState()
     data object Home: MainState()
 }
