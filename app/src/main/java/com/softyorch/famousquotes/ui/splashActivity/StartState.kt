c5ee31e872fb5e020f24e95649240a53ffebdbb5
Jorge1 <agullojorge@gmail.com>
2024-05-23 14:31:24 +0200

    Moved

--- app/src/main/java/com/softyorch/famousquotes/ui/splashActivity/StartState.kt
+++
@@ -1,7 +0,0 @@
-package com.softyorch.famousquotes.ui.splashActivity
-
-sealed class StartState {
-    data object TimeToUpdate: StartState()
-    data object Loading: StartState()
-    data object Start: StartState()
-}
