d6d21739e7f713ce6729dbf3cda978afc4de741e
Jorge1 <agullojorge@gmail.com>
2024-05-24 12:05:08 +0200

    Change logic start

--- app/src/main/java/com/softyorch/famousquotes/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/splash/SplashScreen.kt
@@ -16,14 +16,13 @@ import androidx.navigation.NavHostController
 import com.softyorch.famousquotes.R
 import com.softyorch.famousquotes.ui.navigation.NavigationRoutes
 import com.softyorch.famousquotes.utils.appIcon
-import com.softyorch.famousquotes.utils.isSdk31OrUp
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navHost: NavHostController) {
 
     LaunchedEffect(true) {
-        if (isSdk31OrUp) delay(2000L)
+        delay(2000L)
 
         navHost.navigate(NavigationRoutes.HomeScreen.route) {
             navHost.popBackStack(route = NavigationRoutes.SplashScreen.route, true)
@@ -33,7 +32,7 @@ fun SplashScreen(navHost: NavHostController) {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Image(
             painter = appIcon(),
-            modifier = Modifier.clip(shape = CircleShape).size(280.dp),
+            modifier = Modifier.clip(shape = CircleShape).size(180.dp),
             contentDescription = stringResource(R.string.main_content_desc_image)
         )
     }
