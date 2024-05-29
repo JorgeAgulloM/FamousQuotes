1d2f484e5fafcdb32b7757ebf8ccebf5b7599262
Jorge1 <agullojorge@gmail.com>
2024-05-29 07:51:58 +0200

    Added id property

--- app/src/main/java/com/softyorch/famousquotes/data/defaultDatabase/DefaultModel.kt
+++ app/src/main/java/com/softyorch/famousquotes/data/defaultDatabase/DefaultModel.kt
@@ -1,6 +1,7 @@
 package com.softyorch.famousquotes.data.defaultDatabase
 
 data class DefaultModel(
+    val id: String = "0000000000000001",
     val owner: String = "",
     val quote: List<String> = emptyList(),
     val imageUrl: String = "",
