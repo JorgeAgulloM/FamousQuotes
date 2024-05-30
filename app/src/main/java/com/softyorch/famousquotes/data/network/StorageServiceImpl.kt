0a3328837b55f1e38fa7865643870fb2396ac4c3
Jorge1 <agullojorge@gmail.com>
2024-05-30 09:46:49 +0200

    Added download func

--- app/src/main/java/com/softyorch/famousquotes/data/network/StorageServiceImpl.kt
+++ app/src/main/java/com/softyorch/famousquotes/data/network/StorageServiceImpl.kt
@@ -1,6 +1,9 @@
 package com.softyorch.famousquotes.data.network
 
-import android.os.Environment
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment.DIRECTORY_DOWNLOADS
 import com.google.firebase.FirebaseException
 import com.google.firebase.storage.FirebaseStorage
 import com.softyorch.famousquotes.BuildConfig
@@ -8,22 +11,21 @@ import com.softyorch.famousquotes.domain.interfaces.IStorageService
 import com.softyorch.famousquotes.utils.LevelLog.ERROR
 import com.softyorch.famousquotes.utils.LevelLog.INFO
 import com.softyorch.famousquotes.utils.writeLog
-import kotlinx.coroutines.Dispatchers
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeoutOrNull
-import java.io.File
 import javax.inject.Inject
 
 class StorageServiceImpl @Inject constructor(
     private val storage: FirebaseStorage,
+    @ApplicationContext private val context: Context,
 ) : IStorageService {
 
     companion object {
         private const val TIMEOUT: Long = 4000L
     }
 
-    private val appName = BuildConfig.APP_TITLE
+    private val appName = BuildConfig.APP_TITLE.split("_")[2]
 
     override suspend fun getImage(url: String): String? = try {
         withTimeoutOrNull(TIMEOUT) {
@@ -38,30 +40,44 @@ class StorageServiceImpl @Inject constructor(
         null
     }
 
-    override suspend fun downloadImage(imageId: String) {
-        withTimeoutOrNull(5000) {
+    override suspend fun downloadImage(imageId: String, result: (Boolean) -> Unit) {
+        withTimeoutOrNull(30000) {
             try {
+                val imageName = "$appName-$imageId.png"
                 val url = "/images/famous_quotes/historical_quotes/$imageId/$imageId.png"
-                val urlDownload = withContext(Dispatchers.IO) { getImage(url) }
+                val storageRef = storage.reference.child(url)
 
-                if (!urlDownload.isNullOrBlank()) {
-                    val storageRef = storage.reference.child(url)
-
-                    val rootPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
-
-                    val localFile = File(rootPath, "$appName-$imageId.png")
-
-                    storageRef.getFile(localFile).addOnSuccessListener {
-                        writeLog(INFO, "downloadImage -> DownloadImage is Success ImageId: $imageId")
-                    }.addOnFailureListener {
-                        writeLog(ERROR, "Error: downloadImage -> ImageId: $imageId. Error: ${it.cause}")
+                storageRef.downloadUrl.addOnSuccessListener {
+                    writeLog(INFO, "downloadImage-> is Success ImageId: $imageId")
+                    downloadFile(imageName, it.toString()) {
+                        result(true)
                     }
+                }.addOnFailureListener {
+                    writeLog(ERROR, "downloadImage-> ImageId: $imageId. Error: ${it.cause}")
+                    result(false)
                 }
+
             } catch (fex: FirebaseException) {
                 writeLog(ERROR, "Error downloading image. Error: ${fex.cause}")
+                result(false)
             } catch (ex: Exception) {
-                writeLog(ERROR, "Error downloading image. Error: ${ex.cause}")
+                writeLog(ERROR, "Error creating image. Error: ${ex.cause}")
+                result(false)
             }
+        } ?: result(false)
+    }
+
+    private fun downloadFile(fileName: String, url: String?, result: () -> Unit) {
+        val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val uri = Uri.parse(url)
+        val request = DownloadManager.Request(uri).apply {
+            setTitle(fileName)
+            setDescription(fileName)
+            setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            setDestinationInExternalPublicDir(DIRECTORY_DOWNLOADS, fileName)
+            setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
         }
+
+        downloadManager.enqueue(request).apply { result() }
     }
 }
