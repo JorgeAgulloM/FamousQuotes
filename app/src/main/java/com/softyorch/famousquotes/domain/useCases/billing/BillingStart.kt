5122229f60f0840090b448a7fd2a4badf173306a
Jorge1 <agullojorge@gmail.com>
2024-05-30 19:39:36 +0200

    Added new UseCase

---
+++ app/src/main/java/com/softyorch/famousquotes/domain/useCases/billing/BillingStart.kt
@@ -0,0 +1,41 @@
+package com.softyorch.famousquotes.domain.useCases.billing
+
+import com.softyorch.famousquotes.data.network.IBilling
+import com.softyorch.famousquotes.domain.interfaces.IStorageService
+import kotlinx.coroutines.delay
+import org.json.JSONObject
+import javax.inject.Inject
+
+class BillingStart @Inject constructor(
+    private val billing: IBilling,
+    private val storage: IStorageService,
+) {
+    suspend operator fun invoke(): BillingModel = startBilling().let { isConnected ->
+        return if (isConnected) BillingModel(
+            isConnected = isConnected,
+            productsInApp = getAvailableProducts(getImageList()),
+            productsPurchased = queryPurchases()
+        ) else BillingModel.empty()
+    }
+
+    private suspend fun startBilling(): Boolean {
+        var isConnected = false
+        delay(1000)
+        billing.startConnection { isConnected = it }
+        return isConnected
+    }
+
+    private suspend fun getImageList(): List<String> = storage.getImageList() ?: listOf()
+
+    private suspend fun getAvailableProducts(productsInApp: List<String>): List<String> =
+        billing.queryAvailableProducts(productsInApp)
+            .map { it.originalJson.jsonDetailsToProductId() }.toList()
+
+    private suspend fun queryPurchases(): List<String> = billing.queryPurchases()
+        .map { it.originalJson.jsonDetailsToProductId() }.toList()
+
+    private fun String.jsonDetailsToProductId(): String {
+        val jsonObject = JSONObject(this)
+        return jsonObject.getString("productId")
+    }
+}
