0c80339369c701e54e845a1afc85507d6dbba225
Jorge1 <agullojorge@gmail.com>
2024-05-30 19:40:20 +0200

    Added new property

--- app/src/main/java/com/softyorch/famousquotes/domain/interfaces/IStorageService.kt
+++ app/src/main/java/com/softyorch/famousquotes/domain/interfaces/IStorageService.kt
@@ -3,4 +3,5 @@ package com.softyorch.famousquotes.domain.interfaces
 interface IStorageService {
     suspend fun getImage(url: String): String?
     suspend fun downloadImage(imageId: String, result: (Boolean) -> Unit)
+    suspend fun getImageList(): List<String>?
 }
