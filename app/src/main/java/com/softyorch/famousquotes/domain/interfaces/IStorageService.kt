186ce186d3e648b7f8632ebcefb654546390ebee
Jorge1 <agullojorge@gmail.com>
2024-05-30 09:46:26 +0200

    Added parameter

--- app/src/main/java/com/softyorch/famousquotes/domain/interfaces/IStorageService.kt
+++ app/src/main/java/com/softyorch/famousquotes/domain/interfaces/IStorageService.kt
@@ -2,5 +2,5 @@ package com.softyorch.famousquotes.domain.interfaces
 
 interface IStorageService {
     suspend fun getImage(url: String): String?
-    suspend fun downloadImage(imageId: String)
+    suspend fun downloadImage(imageId: String, result: (Boolean) -> Unit)
 }
