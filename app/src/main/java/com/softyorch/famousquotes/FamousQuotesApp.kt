8f474fcad49aef9d2ed751eaf29b0806f73e9d66
Jorge1 <agullojorge@gmail.com>
2024-05-31 21:00:14 +0200

    Added test devices

--- app/src/main/java/com/softyorch/famousquotes/FamousQuotesApp.kt
+++ app/src/main/java/com/softyorch/famousquotes/FamousQuotesApp.kt
@@ -3,7 +3,9 @@ package com.softyorch.famousquotes
 import android.app.Application
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import dagger.hilt.android.HiltAndroidApp
+import java.util.Arrays
 
 @HiltAndroidApp
 class FamousQuotesApp: Application() {
@@ -16,5 +18,6 @@ class FamousQuotesApp: Application() {
         super.onCreate()
         MobileAds.initialize(this){}
         adRequest = AdRequest.Builder().build()
+        RequestConfiguration.Builder().setTestDeviceIds(listOf("593592E028848EA2BF3B8AEDC4D3D8EE", "5B0C38DF42C762016CCFB455D4473887"))
     }
 }
