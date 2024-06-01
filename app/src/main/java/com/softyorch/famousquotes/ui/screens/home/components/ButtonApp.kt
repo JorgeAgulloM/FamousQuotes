724467ebcb875ab3d1d46da9bb8bfa8f83760f46
Jorge1 <agullojorge@gmail.com>
2024-06-01 18:43:52 +0200

    Changed type typography and color btn

--- app/src/main/java/com/softyorch/famousquotes/ui/screens/home/components/ButtonApp.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/home/components/ButtonApp.kt
@@ -2,14 +2,17 @@ package com.softyorch.famousquotes.ui.screens.home.components
 
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.softyorch.famousquotes.ui.theme.MyTypography
 import com.softyorch.famousquotes.ui.theme.SecondaryColor
 
 @Composable
 fun ButtonApp(text: String, primary: Boolean = false, onClick: () -> Unit) {
-    val color = if (primary) SecondaryColor else Color.LightGray
+    val color = if (primary) SecondaryColor else SecondaryColor.copy(alpha = 0.6f)
     val elevation = if (primary) 4.dp else 1.dp
 
     Button(
@@ -17,6 +20,6 @@ fun ButtonApp(text: String, primary: Boolean = false, onClick: () -> Unit) {
         elevation = ButtonDefaults.buttonElevation(defaultElevation = elevation),
         colors = ButtonDefaults.buttonColors().copy(containerColor = color)
     ) {
-        TextInfo(text)
+        Text(text = text, style = MyTypography.bodyLarge)
     }
 }
