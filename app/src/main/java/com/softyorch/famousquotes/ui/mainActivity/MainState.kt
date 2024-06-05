3ca7485e49ee178481a305ae2aa42955b2968393
Jorge1 <agullojorge@gmail.com>
2024-06-05 11:20:12 +0200

    Deleted state

--- app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainState.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainState.kt
@@ -1,7 +1,6 @@
 package com.softyorch.famousquotes.ui.mainActivity
 
 sealed class MainState {
-    data object TimeToUpdate: MainState()
     data object Home: MainState()
     data object Unauthorized: MainState()
 }
