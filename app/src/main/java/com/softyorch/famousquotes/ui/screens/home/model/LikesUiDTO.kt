15fbf7e10ec7d2523e2e5e68f6148c27a4456edc
Jorge1 <agullojorge@gmail.com>
2024-06-05 05:02:43 +0200

    Added id property

--- app/src/main/java/com/softyorch/famousquotes/ui/screens/home/model/LikesUiDTO.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/home/model/LikesUiDTO.kt
@@ -3,9 +3,10 @@ package com.softyorch.famousquotes.ui.screens.home.model
 import com.softyorch.famousquotes.domain.model.LikesDTO
 
 data class LikesUiDTO(
+    val id: String,
     val isLike: Boolean
 ) {
     companion object {
-        fun LikesUiDTO.toDomain(): LikesDTO = LikesDTO(isLike = this.isLike)
+        fun LikesUiDTO.toDomain(): LikesDTO = LikesDTO(id, isLike)
     }
 }
