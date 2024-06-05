4d778ce346b194c711fab4736e14cc9811adcd53
Jorge1 <agullojorge@gmail.com>
2024-06-05 05:02:51 +0200

    Added id property

--- app/src/main/java/com/softyorch/famousquotes/domain/model/LikesDTO.kt
+++ app/src/main/java/com/softyorch/famousquotes/domain/model/LikesDTO.kt
@@ -1,13 +1,12 @@
 package com.softyorch.famousquotes.domain.model
 
 import com.softyorch.famousquotes.data.network.dto.LikesDataDTO
-import com.softyorch.famousquotes.domain.utils.getTodayId
 
 data class LikesDTO(
+    val id: String,
     val isLike: Boolean
 ) {
    companion object {
-       fun LikesDTO.toData(): LikesDataDTO = LikesDataDTO(getId(), isLike)
-       private fun getId() = getTodayId()
+       fun LikesDTO.toData(): LikesDataDTO = LikesDataDTO(id, isLike)
    }
 }
