b9749758a87c07966bfb63f2467e79138126b437
Jorge1 <agullojorge@gmail.com>
2024-06-05 08:03:07 +0200

    Adapted HomeActions for to be use in Analytics

--- app/src/main/java/com/softyorch/famousquotes/ui/screens/home/HomeActions.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/home/HomeActions.kt
@@ -1,17 +1,17 @@
 package com.softyorch.famousquotes.ui.screens.home
 
-sealed class HomeActions {
-    data object Info: HomeActions()
-    data object New: HomeActions()
-    data object Send: HomeActions()
-    data object Buy: HomeActions()
-    data object Owner: HomeActions()
-    data object Like: HomeActions()
-    data object ShowImage: HomeActions()
-    data object ShowNoConnectionDialog: HomeActions()
-    data object ReConnection: HomeActions()
-    data object DownloadImage: HomeActions()
-    data object ShowToastDownload: HomeActions()
-    data object CloseDialogDownLoadImageAgain: HomeActions()
-    data object SureDownloadImageAgain: HomeActions()
+sealed class HomeActions(val select: String) {
+    data class Info(val action: String = "Info"): HomeActions(action)
+    data class New(val action: String = "New"): HomeActions(action)
+    data class Send(val action: String = "Send"): HomeActions(action)
+    data class Buy(val action: String = "Buy"): HomeActions(action)
+    data class Owner(val action: String = "Owner"): HomeActions(action)
+    data class Like(val action: String = "Like"): HomeActions(action)
+    data class ShowImage(val action: String = "ShowImage"): HomeActions(action)
+    data class ShowNoConnectionDialog(val action: String = "ShowNoConnectionDialog"): HomeActions(action)
+    data class ReConnection(val action: String = "ReConnection"): HomeActions(action)
+    data class DownloadImage(val action: String = "DownloadImage"): HomeActions(action)
+    data class ShowToastDownload(val action: String = "ShowToastDownload"): HomeActions(action)
+    data class CloseDialogDownLoadImageAgain(val action: String = "CloseDialogDownLoadImageAgain"): HomeActions(action)
+    data class SureDownloadImageAgain(val action: String = "SureDownloadImageAgain"): HomeActions(action)
 }
