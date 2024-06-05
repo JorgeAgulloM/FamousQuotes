b44f5c9a807f5c9367d8ed90bad57b29980ab944
Jorge1 <agullojorge@gmail.com>
2024-06-05 05:02:26 +0200

    Added random id generator

--- app/src/main/java/com/softyorch/famousquotes/domain/utils/Utils.kt
+++ app/src/main/java/com/softyorch/famousquotes/domain/utils/Utils.kt
@@ -2,15 +2,28 @@ package com.softyorch.famousquotes.domain.utils
 
 import java.util.Calendar
 import java.util.TimeZone
+import kotlin.random.Random
 
 fun getTodayId(): String {
+   return getCalendarZero().toString()
+}
+
+fun generateRandomId(): String {
+    val calendar = Calendar.getInstance()
+    val dayOfYearRandom = Random.nextInt(1, 366)
+    calendar.timeInMillis = getCalendarZero()
+    calendar.set(Calendar.DAY_OF_YEAR, dayOfYearRandom)
+    return calendar.timeInMillis.toString()
+}
+
+private fun getCalendarZero(): Long {
     val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
     calendar.set(Calendar.HOUR_OF_DAY, 0)
     calendar.set(Calendar.MINUTE, 0)
     calendar.set(Calendar.SECOND, 0)
     calendar.set(Calendar.MILLISECOND, 0)
     val oneHourInMillis = 1000 * 60 * 60
-    return (calendar.timeInMillis - oneHourInMillis).toString()
+    return (calendar.timeInMillis - oneHourInMillis)
 }
 
 fun versionList(nameVersion: String): List<Int> =
