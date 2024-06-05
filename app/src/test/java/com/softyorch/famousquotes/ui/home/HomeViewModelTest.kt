5c7faa53b7b42651f27f1b0f4e9eb19e0d95bdef
Jorge1 <agullojorge@gmail.com>
2024-06-05 11:22:33 +0200

    (WIP) Update test

--- app/src/test/java/com/softyorch/famousquotes/ui/home/HomeViewModelTest.kt
+++ app/src/test/java/com/softyorch/famousquotes/ui/home/HomeViewModelTest.kt
@@ -3,12 +3,17 @@ package com.softyorch.famousquotes.ui.home
 import com.softyorch.famousquotes.core.Intents
 import com.softyorch.famousquotes.core.InternetConnection
 import com.softyorch.famousquotes.core.Send
+import com.softyorch.famousquotes.domain.interfaces.IBilling
+import com.softyorch.famousquotes.domain.interfaces.IStorageService
 import com.softyorch.famousquotes.domain.model.FamousQuoteModel
 import com.softyorch.famousquotes.domain.model.LikesDTO
 import com.softyorch.famousquotes.domain.model.LikesQuote
 import com.softyorch.famousquotes.domain.useCases.GetTodayQuote
+import com.softyorch.famousquotes.domain.useCases.billing.BillingPurchase
+import com.softyorch.famousquotes.domain.useCases.billing.BillingStart
 import com.softyorch.famousquotes.domain.useCases.quoteLikes.GetQuoteLikes
 import com.softyorch.famousquotes.domain.useCases.quoteLikes.SetQuoteLike
+import com.softyorch.famousquotes.domain.utils.getTodayId
 import com.softyorch.famousquotes.ui.screens.home.HomeActions
 import com.softyorch.famousquotes.ui.screens.home.HomeViewModel
 import com.softyorch.famousquotes.utils.TestUtils
@@ -42,6 +47,15 @@ class HomeViewModelTest {
     private lateinit var setLike: SetQuoteLike
 
     @RelaxedMockK
+    private lateinit var billingStart: BillingStart
+
+    @RelaxedMockK
+    private lateinit var billingLaunch: BillingPurchase
+
+    @RelaxedMockK
+    private lateinit var storage: IStorageService
+
+    @RelaxedMockK
     private lateinit var send: Send
 
     @RelaxedMockK
@@ -61,10 +75,13 @@ class HomeViewModelTest {
             selectQuote = selectQuote,
             getLikes = getLikes,
             setLike = setLike,
+            billingStartUseCase = billingStart,
+            billingLaunchPurchase = billingLaunch,
+            storage = storage,
             dispatcherIO = Dispatchers.Unconfined,
             send = send,
             hasConnection = hasConnection,
-            intents = intents
+            intents = intents,
         )
     }
 
@@ -77,9 +94,11 @@ class HomeViewModelTest {
     @Test
     fun `When start view model then getting Today Quote`() = runTest {
         //Prepare test
+        val id = getTodayId()
         val owner = "SoftYorch"
         val quote = "The test quote"
         val returnQuote = FamousQuoteModel(
+            id = id,
             owner = owner,
             body = quote,
             imageUrl = ""
@@ -100,11 +119,12 @@ class HomeViewModelTest {
     @Test
     fun `When start view model then getting Likes Quote`() = runTest {
         //Prepare test
+        val id = getTodayId()
         val likesQuote = LikesQuote(likes = 2, isLike = true)
         val returnFlowLikes = flowOf(likesQuote)
 
         //Given
-        coEvery { getLikes() } returns returnFlowLikes
+        coEvery { getLikes(id) } returns returnFlowLikes
 
         //When
         launch { homeViewModel.onCreate() }
@@ -118,9 +138,11 @@ class HomeViewModelTest {
     @Test
     fun `When user request new quote then calling Random Quote`() = runTest {
         //Prepare test
+        val id = getTodayId()
         val owner = "SoftYorch"
         val quote = "The test random quote"
         val returnQuote = FamousQuoteModel(
+            id = id,
             owner = owner,
             body = quote,
             imageUrl = ""
@@ -133,7 +155,7 @@ class HomeViewModelTest {
         launch {
             homeViewModel.onCreate()
             homeViewModel.showInterstitialOnlyForTesting()
-            homeViewModel.onActions(HomeActions.New)
+            homeViewModel.onActions(HomeActions.New())
         }
         advanceUntilIdle()
 
@@ -145,13 +167,14 @@ class HomeViewModelTest {
     @Test
     fun `When user push the Like Button then send like to service`() = runTest {
         //Prepare test
-        val likesDTO = LikesDTO(isLike = true)
+        val id = getTodayId()
+        val likesDTO = LikesDTO(id = id, isLike = true)
 
         //Given
         coEvery { setLike(likesDTO) } returns Unit
 
         //When
-        launch { homeViewModel.onActions(HomeActions.Like) }
+        launch { homeViewModel.onActions(HomeActions.Like()) }
         advanceUntilIdle()
 
         //Then
