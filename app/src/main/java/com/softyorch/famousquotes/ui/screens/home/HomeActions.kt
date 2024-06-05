de0a822c50de01c99c2d0b74d559cfc5f70bc826
Jorge1 <agullojorge@gmail.com>
2024-06-05 08:08:20 +0200

    Delete unused property

--- app/src/main/java/com/softyorch/famousquotes/ui/screens/home/HomeActions.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/home/HomeActions.kt
@@ -1,17 +1,17 @@
 package com.softyorch.famousquotes.ui.screens.home
 
-sealed class HomeActions(val select: String) {
-    data class Info(val action: String = "Info"): HomeActions(action)
-    data class New(val action: String = "New"): HomeActions(action)
-    data class Send(val action: String = "Send"): HomeActions(action)
-    data class Buy(val action: String = "Buy"): HomeActions(action)
-    data class Owner(val action: String = "Owner"): HomeActions(action)
-    data class Like(val action: String = "Like"): HomeActions(action)
-    data class ShowImage(val action: String = "ShowImage"): HomeActions(action)
-    data class ShowNoConnectionDialog(val action: String = "ShowNoConnectionDialog"): HomeActions(action)
-    data class ReConnection(val action: String = "ReConnection"): HomeActions(action)
-    data class DownloadImage(val action: String = "DownloadImage"): HomeActions(action)
-    data class ShowToastDownload(val action: String = "ShowToastDownload"): HomeActions(action)
-    data class CloseDialogDownLoadImageAgain(val action: String = "CloseDialogDownLoadImageAgain"): HomeActions(action)
-    data class SureDownloadImageAgain(val action: String = "SureDownloadImageAgain"): HomeActions(action)
+sealed class HomeActions {
+    data class Info(val action: String = "Info"): HomeActions()
+    data class New(val action: String = "New"): HomeActions()
+    data class Send(val action: String = "Send"): HomeActions()
+    data class Buy(val action: String = "Buy"): HomeActions()
+    data class Owner(val action: String = "Owner"): HomeActions()
+    data class Like(val action: String = "Like"): HomeActions()
+    data class ShowImage(val action: String = "ShowImage"): HomeActions()
+    data class ShowNoConnectionDialog(val action: String = "ShowNoConnectionDialog"): HomeActions()
+    data class ReConnection(val action: String = "ReConnection"): HomeActions()
+    data class DownloadImage(val action: String = "DownloadImage"): HomeActions()
+    data class ShowToastDownload(val action: String = "ShowToastDownload"): HomeActions()
+    data class CloseDialogDownLoadImageAgain(val action: String = "CloseDialogDownLoadImageAgain"): HomeActions()
+    data class SureDownloadImageAgain(val action: String = "SureDownloadImageAgain"): HomeActions()
 }
