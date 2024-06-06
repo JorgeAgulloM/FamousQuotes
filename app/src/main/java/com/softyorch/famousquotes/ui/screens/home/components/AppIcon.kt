601204531a863d06630599770dad671dfd352964
Jorge1 <agullojorge@gmail.com>
2024-06-06 11:01:06 +0200

    Deleted unused imports

--- app/src/main/java/com/softyorch/famousquotes/ui/screens/home/components/AppIcon.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/home/components/AppIcon.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.softyorch.famousquotes.BuildConfig
 import com.softyorch.famousquotes.R
 import com.softyorch.famousquotes.ui.theme.SecondaryColor
 
