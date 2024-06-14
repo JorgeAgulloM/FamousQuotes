270975243086310b15454e01311d1dcaf25ac375
Jorge1 <agullojorge@gmail.com>
2024-06-14 16:29:39 +0200

    Created ad state

---
+++ app/src/main/java/com/softyorch/famousquotes/ui/admob/BonifiedAdState.kt
@@ -0,0 +1,11 @@
+package com.softyorch.famousquotes.ui.admob
+
+sealed class BonifiedAdState {
+    data object Loading : BonifiedAdState()
+    data object Showed : BonifiedAdState()
+    data object Reward : BonifiedAdState()
+    data object Impression : BonifiedAdState()
+    data object Clicked : BonifiedAdState()
+    data object Close : BonifiedAdState()
+    data object Error : BonifiedAdState()
+}
