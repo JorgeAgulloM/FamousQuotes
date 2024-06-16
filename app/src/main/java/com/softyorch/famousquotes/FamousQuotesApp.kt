0f8d908f1e4de570e667bc6d45891be3753254de
Jorge1 <agullojorge@gmail.com>
2024-06-16 17:10:03 +0200

    Added id samsung ads

--- app/src/main/java/com/softyorch/famousquotes/FamousQuotesApp.kt
+++ app/src/main/java/com/softyorch/famousquotes/FamousQuotesApp.kt
@@ -5,7 +5,6 @@ import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
 import dagger.hilt.android.HiltAndroidApp
-import java.util.Arrays
 
 @HiltAndroidApp
 class FamousQuotesApp: Application() {
@@ -18,6 +17,6 @@ class FamousQuotesApp: Application() {
         super.onCreate()
         MobileAds.initialize(this){}
         adRequest = AdRequest.Builder().build()
-        RequestConfiguration.Builder().setTestDeviceIds(listOf("593592E028848EA2BF3B8AEDC4D3D8EE", "5B0C38DF42C762016CCFB455D4473887"))
+        RequestConfiguration.Builder().setTestDeviceIds(listOf("593592E028848EA2BF3B8AEDC4D3D8EE", "5B0C38DF42C762016CCFB455D4473887", "9BAE37CC1123C50AD21A352FC5573000"))
     }
 }
