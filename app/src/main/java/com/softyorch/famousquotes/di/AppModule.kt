f7287dadafb5114c86a1ede3561edd0f07446ea0
Jorge1 <agullojorge@gmail.com>
2024-07-01 06:48:47 +0200

    Provide injection ISend

--- app/src/main/java/com/softyorch/famousquotes/di/AppModule.kt
+++ app/src/main/java/com/softyorch/famousquotes/di/AppModule.kt
@@ -1,8 +1,12 @@
 package com.softyorch.famousquotes.di
 
+import android.content.Context
+import com.softyorch.famousquotes.core.ISend
+import com.softyorch.famousquotes.core.SendImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -15,4 +19,11 @@ object AppModule {
     @Singleton
     @Provides
     fun providesDispatcherIO(): CoroutineDispatcher = Dispatchers.IO
+
+    @Singleton
+    @Provides
+    fun providesSendInterface(
+        @ApplicationContext context: Context,
+        dispatcherIO: CoroutineDispatcher,
+    ): ISend = SendImpl(context, dispatcherIO)
 }
