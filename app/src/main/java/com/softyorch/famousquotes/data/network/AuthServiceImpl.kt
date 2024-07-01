f405c04670a62d6135e8e29b860856ae1d4d6568
Jorge1 <agullojorge@gmail.com>
2024-07-01 09:17:36 +0200

    Implemented new writeLog error

--- app/src/main/java/com/softyorch/famousquotes/data/network/AuthServiceImpl.kt
+++ app/src/main/java/com/softyorch/famousquotes/data/network/AuthServiceImpl.kt
@@ -3,6 +3,8 @@ package com.softyorch.famousquotes.data.network
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthException
 import com.softyorch.famousquotes.domain.interfaces.IAuthService
+import com.softyorch.famousquotes.utils.LevelLog
+import com.softyorch.famousquotes.utils.writeLog
 import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
 import kotlin.coroutines.resume
@@ -15,12 +17,19 @@ class AuthServiceImpl @Inject constructor(private val auth: FirebaseAuth) : IAut
                 auth.signInAnonymously().addOnSuccessListener {
                     cancelableCoroutine.resume(true)
                 }.addOnFailureListener { ex ->
+                    writeException(ex)
                     cancelableCoroutine.resumeWithException(ex)
                 }
             } catch (fex: FirebaseAuthException) {
+                writeException(fex)
                 cancelableCoroutine.resumeWithException(fex)
             } catch (ex: Exception) {
+                writeException(ex)
                 cancelableCoroutine.resumeWithException(ex)
             }
         }
+
+    private fun writeException(throwable: Throwable) {
+        writeLog(LevelLog.ERROR, "Error when get anonymous auth", throwable)
+    }
 }
