33fb001d0df99a62725f4933cd9675c321269222
Jorge1 <agullojorge@gmail.com>
2024-11-19 20:01:58 +0100

    Implemented navigation Routes

---
+++ app/src/main/java/com/softyorch/famousquotes/ui/core/navigation/Routes.kt
@@ -0,0 +1,12 @@
+package com.softyorch.famousquotes.ui.core.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Splash
+
+@Serializable
+object Home
+
+@Serializable
+object User
