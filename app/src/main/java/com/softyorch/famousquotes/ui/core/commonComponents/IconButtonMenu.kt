5047cfa9032575986e6468c5f20dba6d8af7f2a1
Jorge1 <agullojorge@gmail.com>
2024-12-03 18:21:49 +0100

    New IconButton

--- app/src/main/java/com/softyorch/famousquotes/ui/core/commonComponents/IconButtonMenu.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/core/commonComponents/IconButtonMenu.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.softyorch.famousquotes.ui.theme.SecondaryColor
+import com.softyorch.famousquotes.ui.theme.WhiteSmoke
 
 @Composable
 fun IconButtonMenu(
@@ -23,24 +24,25 @@ fun IconButtonMenu(
     color: Color = SecondaryColor,
     isVisible: Boolean = true,
     isEnabled: Boolean = true,
-    shadowOn: Boolean = false,
+    shadowOn: Boolean = true,
+    isSelected: Boolean = false,
     onClick: () -> Unit,
 ) {
+    val selectColor = if (isSelected) WhiteSmoke.copy(alpha = 0.4f)
+    else WhiteSmoke
+
     if (isVisible) IconButton(
         onClick = { onClick() },
-        colors = IconButtonDefaults.iconButtonColors(contentColor = color),
-        modifier = Modifier.padding(end = 4.dp),
+        colors = IconButtonDefaults.iconButtonColors(contentColor = selectColor),
+        modifier = Modifier
+            .padding(end = 6.dp)
+            .background(color = color, shape = MaterialTheme.shapes.medium),
         enabled = isEnabled
     ) {
         Box(contentAlignment = Alignment.Center) {
-            if (shadowOn) Box(
-                modifier = Modifier
-                    .background(
-                        color = Color.Black.copy(alpha = 0.6f),
-                        shape = CircleShape
-                    )
-                    .size(40.dp)
-            )
+            if (shadowOn) {
+                Box(modifier = Modifier.background(color = color).size(40.dp))
+            }
             Icon(
                 imageVector = icon,
                 contentDescription = cDescription,
