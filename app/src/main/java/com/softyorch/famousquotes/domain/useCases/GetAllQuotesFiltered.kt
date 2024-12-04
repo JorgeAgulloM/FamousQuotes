e99172e9976eab680a465966166537b504532e74
Jorge1 <agullojorge@gmail.com>
2024-12-04 21:02:02 +0100

    (WIP) Implemented filter options

--- app/src/main/java/com/softyorch/famousquotes/domain/useCases/GetAllQuotesFiltered.kt
+++ app/src/main/java/com/softyorch/famousquotes/domain/useCases/GetAllQuotesFiltered.kt
@@ -1,12 +1,13 @@
 package com.softyorch.famousquotes.domain.useCases
 
+import com.softyorch.famousquotes.core.FilterQuotes
 import com.softyorch.famousquotes.domain.interfaces.IDatabaseService
 import com.softyorch.famousquotes.domain.interfaces.IStorageService
 import com.softyorch.famousquotes.domain.model.FamousQuoteModel
 import com.softyorch.famousquotes.domain.model.FamousQuoteModel.Companion.toDomain
 import com.softyorch.famousquotes.ui.screens.home.HomeViewModel.Companion.HTTP
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 // WIP
@@ -14,8 +15,10 @@ class GetAllQuotesFiltered @Inject constructor(
     private val dbService: IDatabaseService,
     private val storageService: IStorageService
 ) {
-    suspend fun invoke(): Flow<List<FamousQuoteModel?>?> = flowOf(
-        dbService.getFavoriteQuotes()?.map { quote ->
+    fun invoke(filter: FilterQuotes): Flow<List<FamousQuoteModel?>?> = flow {
+        val getQuotes = getFunctionService(filter)
+
+        val flow = getQuotes?.map { quote ->
             quote?.let { quoteNotNull ->
                 getImage(quoteNotNull.imageUrl).let { image ->
                     quoteNotNull.copy(imageUrl = image)
@@ -24,7 +27,16 @@ class GetAllQuotesFiltered @Inject constructor(
         }?.filter {
             it?.imageUrl?.startsWith(HTTP) == true
         }
-    )
+
+        emit(flow)
+    }
+
+    private suspend fun getFunctionService(filter: FilterQuotes) =
+        when (filter) {
+            FilterQuotes.Likes -> dbService.getFavoriteQuotes()
+            FilterQuotes.Seen -> dbService.getQuotesShown()
+            FilterQuotes.Favorites -> dbService.getFavoriteQuotes()
+        }
 
     private suspend fun getImage(url: String): String = storageService.getImage(url) ?: url
 
