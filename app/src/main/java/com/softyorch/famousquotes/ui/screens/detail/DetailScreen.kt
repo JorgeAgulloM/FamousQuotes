63ba3d58c62ab8b971727009fbf52bf82a94892e
Jorge1 <agullojorge@gmail.com>
2024-12-11 19:22:13 +0100

    [WIP] Getting data quote.

--- app/src/main/java/com/softyorch/famousquotes/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/detail/DetailScreen.kt
@@ -1,7 +1,19 @@
 package com.softyorch.famousquotes.ui.screens.detail
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.softyorch.famousquotes.domain.model.FamousQuoteModel
+import com.softyorch.famousquotes.ui.screens.grid.components.CardItem
+import com.softyorch.famousquotes.ui.theme.BackgroundColor
 
 @Composable
 fun DetailScreen(
@@ -10,5 +22,21 @@ fun DetailScreen(
     viewModel: DetailViewModel,
     onBackNavigation: () -> Unit
 ) {
+
+    val quote: FamousQuoteModel by viewModel.quote.collectAsStateWithLifecycle()
+
+    LaunchedEffect(true) { viewModel.getQuote(id) }
+
+    Scaffold(modifier = modifier.fillMaxSize(), containerColor = BackgroundColor) { paddingValues ->
+        Column(
+            modifier = modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CardItem(quote) { onBackNavigation() }
+        }
+    }
 
 }
--- app/src/main/java/com/softyorch/famousquotes/ui/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/detail/DetailViewModel.kt
@@ -1,9 +1,35 @@
 package com.softyorch.famousquotes.ui.screens.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softyorch.famousquotes.domain.model.FamousQuoteModel
+import com.softyorch.famousquotes.domain.useCases.GetQuoteById
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailViewModel @Inject constructor(): ViewModel() {
+class DetailViewModel @Inject constructor(
+    private val getQuoteById: GetQuoteById,
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
+): ViewModel() {
+
+    private val _quote = MutableStateFlow(FamousQuoteModel.emptyModel())
+    val quote: StateFlow<FamousQuoteModel> = _quote
+
+    fun getQuote(id: String) {
+        viewModelScope.launch {
+            val result = withContext(defaultDispatcher) {
+                getQuoteById(id)
+            }
+            result?.let { quoteResult -> _quote.update { quoteResult } }
+        }
+    }
+
 }
