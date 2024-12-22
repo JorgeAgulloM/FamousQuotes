1a89cc90d85b1d5514d22129935042d69ad7d00d
Jorge1 <agullojorge@gmail.com>
2024-12-22 13:20:03 +0100

    Added message to user when him don't have any quote on lists.

---
+++ app/src/main/java/com/softyorch/famousquotes/ui/core/commonComponents/MessageToUser.kt
@@ -0,0 +1,42 @@
+package com.softyorch.famousquotes.ui.core.commonComponents
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import com.softyorch.famousquotes.ui.screens.home.components.SpacerHeight
+import com.softyorch.famousquotes.ui.screens.home.components.SpacerWidth
+import com.softyorch.famousquotes.ui.theme.PrimaryColor
+import com.softyorch.famousquotes.ui.theme.WhiteSmoke
+
+@Composable
+fun MessageToUser(
+    icon: ImageVector,
+    msg: String,
+    tint: Color = PrimaryColor,
+    contentDescription: String? = null
+) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Row (
+            modifier = Modifier
+                .background(color = WhiteSmoke, shape = MaterialTheme.shapes.large)
+                .padding(top = 16.dp, start = 8.dp, end = 16.dp, bottom = 16.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(imageVector = icon, contentDescription = contentDescription, tint = tint)
+            Text(text = msg, style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+}
--- app/src/main/java/com/softyorch/famousquotes/ui/screens/grid/GridScreen.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/screens/grid/GridScreen.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.PriorityHigh
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -25,9 +27,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.softyorch.famousquotes.R
+import com.softyorch.famousquotes.core.FilterQuotes
 import com.softyorch.famousquotes.ui.admob.Banner
+import com.softyorch.famousquotes.ui.core.commonComponents.MessageToUser
 import com.softyorch.famousquotes.ui.screens.grid.components.CardItem
 import com.softyorch.famousquotes.ui.screens.grid.components.TopBarGrid
 import com.softyorch.famousquotes.ui.screens.home.components.SpacerHeight
@@ -85,7 +91,7 @@ fun GridScreen(
                 contentAlignment = Alignment.Center
             ) {
                 CircularProgressIndicator()
-            } else LazyVerticalGrid(
+            } else if (allQuotes.isNotEmpty()) LazyVerticalGrid(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues.apply { calculateTopPadding() + 16.dp }),
@@ -104,6 +110,17 @@ fun GridScreen(
                 item {
                     SpacerHeight(Banner.heightBanner)
                 }
+            } else {
+                val messageFromResource = when (selectedQuotes) {
+                    FilterQuotes.Likes -> R.string.have_not_like_quotes
+                    FilterQuotes.Shown -> R.string.have_not_show_any_quotes
+                    FilterQuotes.Favorites -> R.string.have_not_favorites_quotes
+                }
+
+                MessageToUser(
+                    icon = Icons.Rounded.PriorityHigh,
+                    msg = stringResource(messageFromResource)
+                )
             }
         }
     }
