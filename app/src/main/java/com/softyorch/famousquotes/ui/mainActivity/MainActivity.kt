474b8d4ad7918d787ae5e401ade7dc0735eee64b
Jorge1 <agullojorge@gmail.com>
2025-01-03 20:36:26 +0100

    Refactored get notification permission.

--- app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/com/softyorch/famousquotes/ui/mainActivity/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.softyorch.famousquotes.ui.mainActivity
 
 import android.Manifest
+import android.app.AlertDialog
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.os.Build
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.getValue
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -34,6 +38,7 @@ import com.softyorch.famousquotes.R
 import com.softyorch.famousquotes.core.FIREBASE_NOTIFICATION_CHANNEL_1
 import com.softyorch.famousquotes.ui.core.commonComponents.LoadingCircle
 import com.softyorch.famousquotes.ui.screens.main.MainApp
+import com.softyorch.famousquotes.ui.utils.DialogCloseAction
 import com.softyorch.famousquotes.utils.LevelLog
 import com.softyorch.famousquotes.utils.RequestGrantedProtectionData
 import com.softyorch.famousquotes.utils.sdk33AndUp
@@ -69,7 +74,7 @@ class MainActivity : ComponentActivity() {
 
         StartUpdateManager()
         StartFirebase()
-        sdk33AndUp { PermissionNotifications() } ?: SubscribeNotificationFirstTime()
+        PermissionNotifications()
         RequestGrantedProtectionData(this).getConsent()
         SetBlockedScreenShoot()
 
@@ -90,21 +95,53 @@ class MainActivity : ComponentActivity() {
         if (!BuildConfig.DEBUG) checkForAppUpdates()
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private fun PermissionNotifications() {
-        val requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted ->
-            if (isGranted) {
-                writeLog(LevelLog.INFO, "PERMISSION POST_NOTIFICATIONS GRANTED")
-                CreatedChannelNotifications()
-                SubscribeNotificationFirstTime()
-            } else {
-                writeLog(LevelLog.WARN, "PERMISSION POST_NOTIFICATIONS DENIED")
-            }
-        }
+    fun PermissionNotifications(fromSettings: Boolean = false, onResult: (Boolean) -> Unit = {}) {
+            sdk33AndUp {
+                if (fromSettings) {
+                    val permissionStatus = ContextCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    )
 
-        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    when (permissionStatus) {
+                        PackageManager.PERMISSION_GRANTED -> {
+                            writeLog(LevelLog.INFO, "PERMISSION POST_NOTIFICATIONS GRANTED")
+                            onResult(true)
+                        }
+
+                        PackageManager.PERMISSION_DENIED -> {
+                            writeLog(LevelLog.WARN, "PERMISSION POST_NOTIFICATIONS DENIED")
+                            onResult(false)
+                        }
+                    }
+                } else {
+                    val requestPermissionLauncher = registerForActivityResult(
+                        ActivityResultContracts.RequestPermission()
+                    ) { isGranted ->
+                        if (isGranted) {
+                            writeLog(LevelLog.INFO, "PERMISSION POST_NOTIFICATIONS GRANTED")
+                            CreatedChannelNotifications()
+                            StartProcessSuscribeNotifications()
+                        } else {
+                            writeLog(LevelLog.WARN, "PERMISSION POST_NOTIFICATIONS DENIED")
+                        }
+                    }
+
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            } ?: SubscribeNotificationFirstTime().also { onResult(true) }
+    }
+
+    private fun StartProcessSuscribeNotifications() {
+        AreNotificationsEnabled { result ->
+            if (result) SubscribeNotificationFirstTime()
+            else ShowDialogNotificationsDisabled()
+        }
+    }
+
+    private fun AreNotificationsEnabled(onResult: (Boolean) -> Unit) {
+        val notificationManager = NotificationManagerCompat.from(this)
+        onResult(notificationManager.areNotificationsEnabled())
     }
 
     private fun CreatedChannelNotifications() {
@@ -119,6 +156,8 @@ class MainActivity : ComponentActivity() {
 
     private fun SubscribeNotificationFirstTime() {
         var finished = false
+        val delay = 5000L
+        var retry = 0
         val messaging = FirebaseMessaging.getInstance()
 
         lifecycleScope.launch {
@@ -129,12 +168,80 @@ class MainActivity : ComponentActivity() {
                         finished = true
                     } else {
                         writeLog(LevelLog.ERROR, text = "Error subscribing to $FIREBASE_NOTIFICATION_CHANNEL_1")
-                        writeLog(LevelLog.ERROR, text = "Retry in 5 seconds...")
+                        writeLog(LevelLog.ERROR, text = "Times $retry. Retry in 5 seconds...")
+                        finished = retry >= 3
+                        retry += 1
                     }
+                }.addOnFailureListener {
+                    writeLog(LevelLog.ERROR, text = "Error subscribing to $FIREBASE_NOTIFICATION_CHANNEL_1")
+                    writeLog(LevelLog.ERROR, text = "Times $retry. Retry in 5 seconds...")
+                    finished = retry >= 3
+                    retry += 1
                 }
-                delay(5000)
+                delay(delay)
             }
         }
+    }
+
+    private fun ShowDialogNotificationsDisabled() {
+        //Esto no sirve, hay que lanzad un dialogo standar.
+        showDialog(
+            title = "Notificaciones deshabilitadas",
+            message = "Las notificaciones están deshabilitadas para esta aplicación. Por favor, actívalas en la configuración del sistema para recibir actualizaciones importantes.",
+            positiveButtonText = "Ir a configuración",
+            negativeButtonText = "Cancelar"
+        ) { action ->
+            when (action) {
+                DialogCloseAction.POSITIVE -> GoToConfigurationNotifications()
+                DialogCloseAction.NEGATIVE -> ShowConfirmDeniedDialog()
+                DialogCloseAction.DISMISS -> Unit
+            }
+        }
+    }
+
+    private fun GoToConfigurationNotifications() {
+        val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+            putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+        }
+        startActivity(intent)
+    }
+
+    private fun ShowConfirmDeniedDialog() {
+        showDialog(
+            title = "Confirmar bloqueo de notificaciones",
+            message = "Entendemos que hayas decidido no activar las notificaciones en este momento. Ten en cuenta que no recibirás alertas ni actualizaciones importantes relacionadas con la aplicación. Si cambias de opinión, siempre podrás habilitarlas más adelante desde la configuración del sistema.",
+            positiveButtonText = "Confirmar",
+            negativeButtonText = "Vale, quiero activarlas"
+        ) { action ->
+            when (action) {
+                DialogCloseAction.POSITIVE -> Unit
+                DialogCloseAction.NEGATIVE -> GoToConfigurationNotifications()
+                DialogCloseAction.DISMISS -> Unit
+            }
+        }
+    }
+
+    private fun showDialog(
+        title: String,
+        message: String,
+        positiveButtonText: String,
+        negativeButtonText: String,
+        onAction: (DialogCloseAction) -> Unit
+    ) {
+        val dialog = AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(positiveButtonText) { dialog, _ ->
+                onAction(DialogCloseAction.POSITIVE)
+                dialog.dismiss()
+            }
+            .setNegativeButton(negativeButtonText) { dialog, _ ->
+                onAction(DialogCloseAction.NEGATIVE)
+                dialog.dismiss()
+            }
+            .create()
+
+        dialog.show()
     }
 
     override fun onResume() {
